9d94d37b2954c895d2840cf56cc3bcaa534d14ac
Matteo Zini <matteozini96@gmail.com>
2020-07-05 18:30:29 +0200

    Added code for coordinates and manipulability plots

--- Robotics.m
+++ Robotics.m
@@ -21,7 +21,8 @@ robot = SerialLink(L, 'name', 'Robot');
 
 %"manip" for manipulability optimization
 %"limits" for workspace limits optimization
-optimization = "none";
+%"noopt" for no optimization
+optimization = "limits";
 
 
 n_joints = size(robot.qlim, 1);
@@ -39,6 +40,9 @@ Ttraj = ctraj(T_start, T_end, n_steps);
 
 q_traj(1,1:7) = q_start;
 q_traj_noopt(1, 1:7) = q_start;
+
+manip_traj(1) = manipulability(0, q_start, robot, 0, 0);
+manip_traj_noopt(1) = manipulability(0, q_start, robot, 0, 0);
 
 q_current = q_start;
 
@@ -76,34 +80,61 @@ for i = 1:(n_steps-1)
 	 
 	dq_dot_curr = J_pinv * dVe';
 	q_current = q_current + (dq_dot_curr * dt);
-	q_traj_noopt(i+1, 1:7) = q_current;
-
-	if optimization == "manip"
-		lb = robot.qlim(:,1) - q_current;
-		ub = robot.qlim(:,2) - q_current;
-		
-		options = optimoptions('fmincon', 'Display', 'off');
-		obj_fun = @(x) -manipulability(x, q_current, robot, dt, P);
-		dq0_dot = fmincon(obj_fun, zeros(n_joints, 1), [], [], [], [], lb, ub, [], options);
-		q_current = q_current + (P * dq0_dot) * dt;
-	elseif optimization == "limits"
-		lb = robot.qlim(:,1) - q_current;
-		ub = robot.qlim(:,2) - q_current;
-		
-		options = optimoptions('fmincon', 'Display', 'off');
-		obj_fun = @(x) -dist_from_limit(x, q_current, robot, dt, P);
-		dq0_dot = fmincon(obj_fun, zeros(n_joints, 1), [], [], [], [], lb, ub, [], options);
-		q_current = q_current + (P * dq0_dot) * dt;
-	end
 	
 	q_traj(i+1, 1:7) = q_current;
+	manip_traj(i+1) = manipulability(0, q_current, robot, 0, 0);
+
+	if optimization ~= "noopt"
+		
+		lb = robot.qlim(:,1) - q_current;
+		ub = robot.qlim(:,2) - q_current;
+		
+		options = optimoptions('fmincon', 'Display', 'off');
+
+		if optimization == "manip"
+			obj_fun = @(x) -manipulability(x, q_current, robot, dt, P);
+		elseif optimization == "limits"
+			obj_fun = @(x) -dist_from_limit(x, q_current, robot, dt, P);
+		end
+		
+		dq0_dot = fmincon(obj_fun, zeros(n_joints, 1), [], [], [], [], lb, ub, [], options);
+		q_current = q_current + (P * dq0_dot) * dt;
+			
+		q_traj(i+1, 1:7) = q_current;
+		manip_traj(i+1) = manipulability(0, q_current, robot, 0, 0);
+	
+	else
+		q_traj_noopt(i+1, 1:7) = q_current;
+		manip_traj_noopt(i+1) = manipulability(0, q_current, robot, 0, 0);
+	end
+	
+
 end
 
-T_traj = robot.fkine(q_traj)
-T_traj_noopt = robot.fkine(q_traj_noopt)
+%T_traj = robot.fkine(q_traj)
+%T_traj_noopt = robot.fkine(q_traj_noopt)
 
 
-robot.plot(q_traj);
+%robot.plot(q_traj);
+
+%ellipsoid_matrix(:,:) = manip_matrix_traj(1,:,:)
+%plot_ellipse(ellipsoid_matrix);
+
+
+%% Q PLOTS (OPT vs NO_OPT)
+
+for i = 1:n_links
+	figure(i)
+	plot(1:50, q_traj(:,i), 'b');
+	hold on
+	plot(1:50, q_traj_noopt(:,i), 'r');
+end
+
+%% MANIPULABILITY PLOT
+
+	plot(1:50, manip_traj(:), 'b');
+	hold on
+	plot(1:50, manip_traj_noopt(:), 'r');
 
 
 %% OBJECTIVE FUNCTIONS
@@ -134,3 +165,13 @@ function lim_dist = dist_from_limit(dq0_dot, q, robot, dt, P)
 	lim_dist = avg_sum / (2 * size(q_updated, 1));
 end
 
+
+%% OTHER UTILITY FUNCTIONS
+
+function M = manipulab_ellipsoid_matrix(robot, q)
+
+	J = robot.jacob0(q);
+	
+	M = inv(J(1:3,:)*J(1:3,:)');
+
+end
